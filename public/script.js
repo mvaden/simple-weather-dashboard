51ca7646d6a46bd654f8161e1fb390b0b137b1bb
Matthew Vaden <matthewvaden@gmail.com>
2020-08-19 02:46:14 -0400

    Return data to the client.

--- public/script.js
+++ public/script.js
@@ -26,3 +26,30 @@ searchBox.addListener('places_changed', () => {
     })
     .catch(err => console.error(err, "THE FETCH FAILED"))
 });
+
+const dataAttributes = {
+    condition:  'condition',
+    location:   'location',
+    pressure:   'pressure',
+    temp_f:     'temperature',
+    wind_dir:   'wind_dir',
+    wind_mph:   'wind_mph'
+}
+
+const DOMOutputs = {
+    pressure:           document.getElementById(dataAttributes.pressure),
+    currentCondition:   document.getElementById(dataAttributes.condition),
+    currentTemperature: document.getElementById(dataAttributes.temp_f),
+    currentWind:        document.getElementById(dataAttributes.wind_mph),
+    location:           document.getElementById(dataAttributes.location),
+    wind_dir:           document.getElementById(dataAttributes.wind_dir)
+}
+
+setWeatherData = (data, name) => {
+    DOMOutputs.currentWind.textContent          = 'Wind: ' + `${Math.round(data.wind_mph)} mph`,
+    DOMOutputs.currentTemperature.textContent   = 'Temperature: ' + `${Math.round(data.temp_f)}°F`,
+    DOMOutputs.currentCondition.textContent     = data.condition.text,
+    DOMOutputs.wind_dir.textContent             = data.wind_dir,
+    DOMOutputs.pressure.textContent             = 'Pressure: ' + data.pressure_in + 'in',
+    DOMOutputs.location.textContent             = 'in ' + name
+}
